a7187dea01c1c274e5362be942fde397013b1955
Felipe Henssler Koetz <feliphenssler@gmail.com>
2023-10-07 08:02:46 -0300

    chore: documentation

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,33 +1,39 @@
 pragma solidity ^0.4.17;
 
 contract Lottery {
-    address public manager;
-    address[] public players;
+    address public manager; // Seve the manager address
+    address[] public players; // Save the players address
     
+    // Constructor
     function Lottery() public {
         manager = msg.sender;
     }
     
+    // Enter the lottery, must send more than .01 ether - transaction
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
     }
     
+    // Return a very big random number
     function random() private view returns (uint) {
         return uint(keccak256(block.difficulty, now, players));
     }
     
+    // Pick a winner for the current lottery and send him all the money, reset the players array - transaction
     function pickWinner() public restricted {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
         players = new address[](0);
     }
     
+    // Modifier to restrict the access to the manager
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
     
+    // Return the players array
     function getPlayers() public view returns (address[]) {
         return players;
     }
--- deploy.js
+++ deploy.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require('@truffle/hdwallet-provider'); //HDWalletProvide unlocks your account to deploy to any Network you want
+const { Web3 } = require('web3'); // Web3 is a library that allows us to interact with the Ethereum network
+const { interface, bytecode } = require('./compile'); // interface is the ABI, bytecode is the actual contract
 
+// We are using the HDWalletProvider to unlock our account and connect to the network
 const provider = new HDWalletProvider(
   process.env.SECRET_WALLET_MNEMONIC,
   process.env.INFURA_URL
 );
 
+// We are using the Web3 library to connect to the network
 const web3 = new Web3(provider);
 
+// We are using the deploy function to deploy our contract to the network
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
